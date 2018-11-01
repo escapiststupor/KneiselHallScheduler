5bac2d594b58723f76398403754262b37775b88f
Phyllis Yen <phyllis@17.media>
2018-11-01 09:06:16 +0800

    save state to localStorage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import throttle from 'lodash/throttle';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import App from './App';
+import { saveState, loadState } from './utils';
 import * as serviceWorker from './serviceWorker';
+
+const persistedState = loadState();
 
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
-  rootReducer /* preloadedState, */,
+  rootReducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 /* eslint-enable */
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 5000)
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,25 @@ import uuidv4 from 'uuid';
 export const getHashID = () => {
   return uuidv4();
 };
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('KH_DB');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.error(err);
+    return undefined;
+  }
+};
+
+export const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('KH_DB', serializedState);
+  } catch (err) {
+    console.error('cannot write to localStorage!');
+  }
+};
