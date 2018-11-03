553ff1b2bb5d5845108e54b68d4bddb74234a3c2
Phyllis Yen <phyllis@17.media>
2018-11-03 15:24:40 +0800

    add/remove actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,12 @@ export const DELETE_COACH = 'DELETE_COACH';
 export const deleteCoach = createAction(DELETE_COACH);
 export const DELETE_ALL_COACHES = 'DELETE_ALL_COACHES';
 export const deleteAllCoaches = createAction(DELETE_ALL_COACHES);
+export const CHANGE_COACH_NAME = 'CHANGE_COACH_NAME';
+export const changeCoachName = createAction(CHANGE_COACH_NAME);
+export const CHANGE_COACH_ROOM = 'CHANGE_COACH_ROOM';
+export const changeCoachRoom = createAction(CHANGE_COACH_ROOM);
+export const CHANGE_COACHING_TIME = 'CHANGE_COACHING_TIME';
+export const changeCoachingTime = createAction(CHANGE_COACHING_TIME);
 
 // for students
 export const ADD_STUDENT = 'ADD_STUDENT';
@@ -15,6 +21,10 @@ export const DELETE_STUDENT = 'DELETE_STUDENT';
 export const deleteStudent = createAction(DELETE_STUDENT);
 export const DELETE_ALL_STUDENTS = 'DELETE_ALL_STUDENTS';
 export const deleteAllStudents = createAction(DELETE_ALL_STUDENTS);
+export const CHANGE_STUDENT_NAME = 'CHANGE_STUDENT_NAME';
+export const changeStudentName = createAction(CHANGE_STUDENT_NAME);
+export const CHANGE_STUDENT_INST = 'CHANGE_STUDENT_INST';
+export const changeStudentInst = createAction(CHANGE_STUDENT_INST);
 
 // for groups
 export const CREATE_GROUP = 'CREATE_GROUP';
@@ -23,15 +33,11 @@ export const DELETE_GROUP = 'DELETE_GROUP';
 export const deleteGroup = createAction(DELETE_GROUP);
 export const DELETE_ALL_GROUPS = 'DELETE_ALL_GROUPS';
 export const deleteAllGroups = createAction(DELETE_ALL_GROUPS);
-export const SET_WORK = 'SET_WORK';
-export const setWork = createAction(SET_WORK);
-export const SET_COACH = 'SET_COACH';
-export const setCoach = createAction(SET_COACH);
-export const SET_MEMBERS = 'SET_MEMBERS';
-export const setMembers = createAction(SET_MEMBERS);
-export const SET_REHEARSAL_PLACE = 'SET_REHEARSAL_PLACE';
-export const setRehearsalPlace = createAction(SET_REHEARSAL_PLACE);
-export const SET_REHEARSAL_TIME = 'SET_REHEARSAL_TIME';
-export const setRehearsalTime = createAction(SET_REHEARSAL_TIME);
-export const SET_COACHING_TIME = 'SET_COACHING_TIME';
-export const setCoachingTime = createAction(SET_COACHING_TIME);
+export const CHANGE_WORK = 'CHANGE_WORK';
+export const changeWork = createAction(CHANGE_WORK);
+export const CHANGE_COACH = 'CHANGE_COACH';
+export const changeCoach = createAction(CHANGE_COACH);
+export const DELETE_STUDENT_FROM_GROUP = 'DELETE_STUDENT_FROM_GROUP';
+export const deleteStudentFromGroup = createAction(DELETE_STUDENT_FROM_GROUP);
+export const ADD_STUDENT_TO_GROUP = 'ADD_STUDENT_TO_GROUP';
+export const addStudentToGroup = createAction(ADD_STUDENT_TO_GROUP);
