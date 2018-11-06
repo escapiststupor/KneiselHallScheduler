1e574fa3895f3efc31cb46c73da98296ba03963e
Phyllis Yen <phyllis@17.media>
2018-11-06 13:23:21 +0800

    Updates

---
+++ precache-manifest.e3f38a72ef947ba0f0f90ab224b41f31.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f4cb1a747dbbd89d3c5",
+    "url": "/kneisel-hall-scheduler/static/js/runtime~main.9f4cb1a7.js"
+  },
+  {
+    "revision": "e17ac68d8d1e7b53f5c8",
+    "url": "/kneisel-hall-scheduler/static/js/main.e17ac68d.chunk.js"
+  },
+  {
+    "revision": "99ebb5fc4b7dc7d09cb4",
+    "url": "/kneisel-hall-scheduler/static/js/1.99ebb5fc.chunk.js"
+  },
+  {
+    "revision": "89d39fc7928c0f5ea0ccb1e55bf93419",
+    "url": "/kneisel-hall-scheduler/index.html"
+  }
+];
